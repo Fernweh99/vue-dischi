9981ecd93bbce97aef9124d0933c4ed9a07b9a1b
Fernweh99 <daniele.dimento@gmail.com>
2022-07-26 16:53:49 +0200

    finish ex

--- src/components/CdCardSection.vue
+++ src/components/CdCardSection.vue
@@ -2,17 +2,15 @@
   <div class="section-cd-card-">
     <div class="container">
       <div class="row g-5 row-cols-5">
-        <div v-for="cd in cds" :key="cd.poster" class="col">
-          <div class="cd-card h-100">
-            <img class="img-fluid" :src="cd.poster" :alt="cd.title">
-            <div class="cd-info text-center">
-              <h4 class="cd-title">{{ cd.title }}</h4>
-              <span class="cd-moreinfo">
-                {{ cd.author }}<br/>
-                {{ cd.year }}
-              </span>
-            </div>
-          </div>
+        <div v-for="cd in cds" class="col" 
+        :key="cd.poster"
+        >
+          <SingleCdCard 
+          :image="cd.poster"
+          :title="cd.title"
+          :author="cd.author"
+          :year="cd.year"
+          />
         </div>
       </div>
     </div>
@@ -21,8 +19,12 @@
 
 <script>
 import axios from "axios";
+import SingleCdCard from "./SingleCdCard.vue"
 export default {
   name: "CdCardSection",
+  components: {
+    SingleCdCard,
+  },
   data() {
     return {
       url: "https://flynn.boolean.careers/exercises/api/array/music",
@@ -43,19 +45,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-  @import "../assets/scss/vars.scss"; 
-  .cd-card {
-    padding: 15px;
-    background-color: $primary_color;
-    .cd-info {
-      .cd-title {
-        text-transform: capitalize;
-        color: white;
-      }
-      .cd-moreinfo {
-        color: gray;
-      }
-    }
-  }
+<style lang="scss">
+
 </style>
---
+++ src/components/SingleCdCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cd-card h-100">
+    <img class="img-fluid" :src="image" :alt="title">
+    <div class="cd-info text-center">
+      <h4 class="cd-title">{{ title }}</h4>
+      <span class="cd-moreinfo">
+        {{ author }}<br/>
+        {{ year }}
+      </span>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: "SingleCdCard",
+  props: {
+    image: String,
+    author: String,
+    title: String,
+    year: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/vars.scss"; 
+  .cd-card {
+    padding: 15px;
+    background-color: $primary_color;
+    .cd-info {
+      .cd-title {
+        text-transform: capitalize;
+        color: white;
+      }
+      .cd-moreinfo {
+        color: gray;
+      }
+    }
+  }
+</style>
